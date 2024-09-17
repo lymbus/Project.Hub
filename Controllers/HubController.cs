88ff542cc439031512f5f55b1066e525d008d37a
Lydia Estevez Chamorro <lydia.estevez-chamorro@sogeti.com>
2024-09-17 18:19:56 +0200

    prueba

--- Controllers/HubController.cs
+++ Controllers/HubController.cs
@@ -4,22 +4,22 @@ namespace Project.Hub.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class HubController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<HubController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public HubController(ILogger<HubController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<WeatherForecast> Search()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
---
+++ Startup.cs
@@ -0,0 +1,42 @@
+namespace Project.Hub
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddLogging();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            c.IncludeXmlComments("obj\\Debug\\net6.0\\Project.WebApi.xml"));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
